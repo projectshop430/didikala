370286c836406465dd3d6cea703f99ebdbcf6cf6
projectshop430 <projectshop430@gmail.com>
2023-01-26 19:54:03 +0330

    commit done fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations' ;
-import { ToastrModule } from 'ngx-toastr';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 import { AppComponent } from './app.component';
@@ -29,11 +28,7 @@ import { FootersignComponent } from './User/Footer/Sign/footersign/footersign.co
     ReactiveFormsModule,
     BrowserAnimationsModule,
     NgxUiLoaderModule,
-    ToastrModule.forRoot({
-      timeOut: 10000,
-      positionClass: 'toast-bottom-right',
-      preventDuplicates: true,
-    }),
+
   ],
 
   providers: [],
