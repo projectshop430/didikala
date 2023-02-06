dbba6630b3423630db5ec510795f248b5b4e10d3
sepideh-raeisi <102673213+sepideh123456789@users.noreply.github.com>
2023-02-06 12:19:00 +0330

    verifyphonenumbersetp1

--- src/app/User/verifyphonenumber/verifyphonenumber/verifyphonenumber.component.ts
+++ src/app/User/verifyphonenumber/verifyphonenumber/verifyphonenumber.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-verifyphonenumber',
   templateUrl: './verifyphonenumber.component.html',
   styleUrls: ['./verifyphonenumber.component.css']
 })
-export class VerifyphonenumberComponent {
+export class VerifyphonenumberComponent  {
+
+confirmationnumber:string=" تایید شماره" ;
+descriptionconfirimation1:string ='  کد تایید ارسال گردید';
+descriptionconfirimation2:string='برای شماره همراه';
+descriptionphonnumber!:number;
+editnumber:string="ویرایش شماره";
+label:string="کد تایید را وارد کنید";
+// ----------------------------------------------//
 
 }
